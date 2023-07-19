09f5359312956b82d1d4be5cdabbb2c5d11e1501
steady-on <roen.steady.on@gmail.com>
2023-07-19 16:38:21 +0900

    feat: Assignment 01
    - Netflix Project에서 재생 버튼을 클릭했을 때 4개의 이미지뷰의 이미지(포스터)가 랜덤으로 변경되도록 구현
    - Set와 randomElement를 사용하여 중복되는 이미지가 들어가지 않도록 함

--- Day 3/Bomflix/Bomflix/MovieViewController.swift
+++ Day 3/Bomflix/Bomflix/MovieViewController.swift
@@ -17,6 +17,8 @@ class MovieViewController: UIViewController {
     
     @IBOutlet var randomPlayButton: UIButton!
     
+    var pickedContents = Set<String>()
+    
     /// 사용자에게 화면이 보이기 직전에 실행되는 부분
     /// 모서리 둥글기, 그림자 등 스토리보드에서 설정할 수 없는 UI를 설정할 때 주로 사용
     /// View Controller 생명주기
@@ -24,16 +26,19 @@ class MovieViewController: UIViewController {
         super.viewDidLoad()
         
         setRandomPosterToMainImage()
-        setInitialImage()
-        setInitialPlayButton()
+        designPlayButton()
         
         designPreviewImageView(outletName: firstRecommandContents, borderColor: UIColor.systemPink.cgColor)
         designPreviewImageView(outletName: secondRecommandContents, borderColor: UIColor.systemGreen.cgColor)
         designPreviewImageView(outletName: thirdRecommandContents, borderColor: UIColor.systemIndigo.cgColor)
+        
+        pickRandomMoviesUp()
+        setImagesToUIImages()
     }
     
     @IBAction func setRandomContentsTapped(_ sender: UIButton) {
-        setRandomPosterToMainImage()
+        pickRandomMoviesUp()
+        setImagesToUIImages()
     }
     
     @IBAction func likedButtonClicked(_ sender: UIButton) {
@@ -51,6 +56,25 @@ class MovieViewController: UIViewController {
         present(alert, animated: true)
     }
     
+    func pickRandomMoviesUp() {
+        pickedContents = []
+        
+        let movies = ["1", "2", "3", "4", "5", "극한직업", "도둑들", "명량", "베테랑", "부산행", "신과함께인과연", "신과함께죄와벌", "아바타", "알라딘", "암살", "어벤져스엔드게임", "왕의남자", "태극기휘날리며", "택시운전사", "해운대"]
+        
+        while pickedContents.count < 4 {
+            let pick = movies.randomElement()!
+            pickedContents.insert(pick)
+        }
+    }
+    
+    func setImagesToUIImages() {
+        let picked = Array(pickedContents)
+        mainContentsImage.image = UIImage(named: picked[0])
+        firstRecommandContents.image = UIImage(named: picked[1])
+        secondRecommandContents.image = UIImage(named: picked[2])
+        thirdRecommandContents.image = UIImage(named: picked[3])
+    }
+    
     func randomMovies() -> String {
         let movies = ["극한직업", "도둑들", "명량", "베테랑", "부산행", "신과함께인과연", "신과함께죄와벌", "아바타", "알라딘", "암살", "어벤져스엔드게임", "왕의남자", "태극기휘날리며", "택시운전사", "해운대"]
         let randomMovie = movies.randomElement()!
@@ -61,21 +85,8 @@ class MovieViewController: UIViewController {
         let randomNumber = Int.random(in: 1...5)
         mainContentsImage.image = UIImage(named: "\(randomNumber)")
     }
-    
-    func setInitialImage() {
-//        firstRecommandContents.setBackgroundImage(UIImage(named: "극한직업"), for: .normal)
-//        firstRecommandContents.contentMode = .scaleAspectFill
         
-        secondRecommandContents.image = UIImage(named: "암살")
-        secondRecommandContents.layer.cornerRadius = 60
-        secondRecommandContents.layer.borderWidth = 2
-        secondRecommandContents.layer.borderColor = UIColor.gray.cgColor
-        
-        secondRecommandContents.backgroundColor = .blue
-        secondRecommandContents.contentMode = .scaleAspectFill
-    }
-    
-    func setInitialPlayButton() {
+    func designPlayButton() {
         //        randomPlayButton.setTitle("재생", for: .normal)
         //        randomPlayButton.setTitle("눌러주세요", for: .highlighted)
                 
@@ -89,8 +100,8 @@ class MovieViewController: UIViewController {
         //        randomPlayButton.setTitleColor(.red, for: .highlighted)
                 
                 randomPlayButton.layer.cornerRadius = 10
-                randomPlayButton.layer.borderColor = UIColor.red.cgColor
-                randomPlayButton.layer.borderWidth = 4
+//                randomPlayButton.layer.borderColor = UIColor.red.cgColor
+//                randomPlayButton.layer.borderWidth = 4
     }
     
     func designPreviewImageView(outletName name: UIImageView, borderColor: CGColor) {
@@ -99,7 +110,5 @@ class MovieViewController: UIViewController {
         name.layer.borderWidth = 5
         name.backgroundColor = .blue
         name.contentMode = .scaleAspectFill
-        
-        name.image = UIImage(named: randomMovies())
     }
 }
