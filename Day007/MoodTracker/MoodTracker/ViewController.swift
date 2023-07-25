c1764dfa8033090ad9c04facb6fa6b85e5906eaf
steady-on <roen.steady.on@gmail.com>
2023-07-25 17:45:32 +0900

    feat: add IBAction to Button
    - 버튼을 탭하면 각 버튼의 tag 값으로 Emotion 객체를 생성
    - 생성된 emotion 객체로 addValue(to:)와 printMessageForEmotionCount(to:) 실행

--- Day007/MoodTracker/MoodTracker/ViewController.swift
+++ Day007/MoodTracker/MoodTracker/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var emotionManager = EmotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func emotionButtonTapped(_ sender: UIButton) {
+        guard let emotionForTagValue = Emotion(rawValue: sender.tag) else {
+            print("Error! 잘못된 입력입니다.")
+            return
+        }
+        
+        emotionManager.addValue(to: emotionForTagValue)
+        emotionManager.printMessageForEmotionCount(to: emotionForTagValue)
+    }
+    
 }
 
