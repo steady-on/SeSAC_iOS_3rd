6e5aff5b7c64f12e5e7995ef212dffc1d3918c7f
steady-on <roen.steady.on@gmail.com>
2023-07-26 01:14:31 +0900

    refaction: setPullDownAction(to:)
    - 각 UIAction의 UIActionHandler에서 매개변수로 들어온 UIButton을 활용하도록 로직 변경
    - 기존에는 UIActionHandler에서 클로저의 매개변수를 사용하여 UIButton으로 sender를 다운캐스팅 하였음

--- Day007/MoodTracker/MoodTracker/ViewController.swift
+++ Day007/MoodTracker/MoodTracker/ViewController.swift
@@ -30,28 +30,22 @@ class ViewController: UIViewController {
     }
     
     func setPullDownAction(to button: UIButton) {
-        let plusFive = UIAction(title: "+5", image: UIImage(systemName: "plus.circle")) { action in
-            guard let sender = action.sender as? UIButton else { return }
-            
-            guard let selectedEmotion = Emotion(rawValue: sender.tag) else { return }
+        let plusFive = UIAction(title: "+5", image: UIImage(systemName: "plus.circle")) { _ in
+            guard let selectedEmotion = Emotion(rawValue: button.tag) else { return }
             
             EmotionManager.addValue(to: selectedEmotion, count: 5)
             EmotionManager.printMessageForEmotionCount(to: selectedEmotion)
         }
         
-        let plusTen = UIAction(title: "+10", image: UIImage(systemName: "plus.circle.fill")) { action in
-            guard let sender = action.sender as? UIButton else { return }
-            
-            guard let selectedEmotion = Emotion(rawValue: sender.tag) else { return }
+        let plusTen = UIAction(title: "+10", image: UIImage(systemName: "plus.circle.fill")) { _ in
+            guard let selectedEmotion = Emotion(rawValue: button.tag) else { return }
             
             EmotionManager.addValue(to: selectedEmotion, count: 10)
             EmotionManager.printMessageForEmotionCount(to: selectedEmotion)
         }
         
-        let resetData = UIAction(title: "기록 초기화", subtitle: "해당 기분의 기록 초기화", image: UIImage(systemName: "trash"), attributes: .destructive) { action in
-            guard let sender = action.sender as? UIButton else { return }
-            
-            guard let selectedEmotion = Emotion(rawValue: sender.tag) else { return }
+        let resetData = UIAction(title: "기록 초기화", subtitle: "해당 기분의 기록 초기화", image: UIImage(systemName: "trash"), attributes: .destructive) { _ in
+            guard let selectedEmotion = Emotion(rawValue: button.tag) else { return }
             
             EmotionManager.resetData(of: selectedEmotion)
             EmotionManager.printMessageForEmotionCount(to: selectedEmotion)
