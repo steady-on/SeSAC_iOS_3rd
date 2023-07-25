e8dee03eb4301eaab451a1b2e054a1fdb0629e6e
steady-on <roen.steady.on@gmail.com>
2023-07-25 18:11:39 +0900

    add: StatisticsViewController
    - StatisticsViewController 생성
    - Storyboard에 연결

---
+++ Day007/MoodTracker/MoodTracker/StatisticsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StatisticsViewController.swift
+//  MoodTracker
+//
+//  Created by Roen White on 2023/07/25.
+//
+
+import UIKit
+
+class StatisticsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
