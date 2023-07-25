a9cc7d72b5d3f3a02e4e1d894a7323e7daffd816
steady-on <roen.steady.on@gmail.com>
2023-07-26 01:09:41 +0900

    feat: setPullDownAction(to:)
    - 버튼을 매개변수로 받아 UIMenu를 추가하는 메서드 작성
    - 기분 카운트를 5 올려주는 plusFive,
    - 기분 카운트를 10 올려주는 plusTen,
    - 기분 카운트를 초기화 하는 resetData UIAction 작성
    - 작성된 UIAction을 바탕으로 Action의 sender를 통해 Emotion 객체를 만들어 EmotionManager의 메서드를 실행하도록 함

--- Day007/MoodTracker/MoodTracker/ViewController.swift
+++ Day007/MoodTracker/MoodTracker/ViewController.swift
@@ -24,5 +24,35 @@ class ViewController: UIViewController {
         EmotionManager.printMessageForEmotionCount(to: emotionForTagValue)
     }
     
+    func setPullDownAction(to button: UIButton) {
+        let plusFive = UIAction(title: "+5", image: UIImage(systemName: "plus.circle")) { action in
+            guard let sender = action.sender as? UIButton else { return }
+            
+            guard let selectedEmotion = Emotion(rawValue: sender.tag) else { return }
+            
+            EmotionManager.addValue(to: selectedEmotion, count: 5)
+            EmotionManager.printMessageForEmotionCount(to: selectedEmotion)
+        }
+        
+        let plusTen = UIAction(title: "+10", image: UIImage(systemName: "plus.circle.fill")) { action in
+            guard let sender = action.sender as? UIButton else { return }
+            
+            guard let selectedEmotion = Emotion(rawValue: sender.tag) else { return }
+            
+            EmotionManager.addValue(to: selectedEmotion, count: 10)
+            EmotionManager.printMessageForEmotionCount(to: selectedEmotion)
+        }
+        
+        let resetData = UIAction(title: "기록 초기화", subtitle: "해당 기분의 기록 초기화", image: UIImage(systemName: "trash"), attributes: .destructive) { action in
+            guard let sender = action.sender as? UIButton else { return }
+            
+            guard let selectedEmotion = Emotion(rawValue: sender.tag) else { return }
+            
+            EmotionManager.resetData(of: selectedEmotion)
+            EmotionManager.printMessageForEmotionCount(to: selectedEmotion)
+        }
+        
+        button.menu = UIMenu(options: .displayInline, children: [plusFive, plusTen, resetData])
+    }
 }
 
