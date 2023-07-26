d5f998293d662a7820345ae61399fb808cab9b1d
steady-on <roen.steady.on@gmail.com>
2023-07-26 15:26:57 +0900

    feat: add property color to Emotion
    추가한 Color Assets를 바탕으로 각 감정에 대한 Color를 프로퍼티로 작성

--- Day007/MoodTracker/MoodTracker/Emotion.swift
+++ Day007/MoodTracker/MoodTracker/Emotion.swift
@@ -5,7 +5,7 @@
 //  Created by Roen White on 2023/07/25.
 //
 
-import Foundation
+import UIKit
 
 enum Emotion: Int, CaseIterable {
     case happy
@@ -26,6 +26,21 @@ enum Emotion: Int, CaseIterable {
             return "속상한"
         case .sad:
             return "너무 슬픈"
+        }
+    }
+    
+    var color: UIColor? {
+        switch self {
+        case .happy:
+            return UIColor(named: "MTRed")
+        case .smile:
+            return UIColor(named: "MTOrange")
+        case .soso:
+            return UIColor(named: "MTYellow")
+        case .upset:
+            return UIColor(named: "MTMint")
+        case .sad:
+            return UIColor(named: "MTNavy")
         }
     }
 }
