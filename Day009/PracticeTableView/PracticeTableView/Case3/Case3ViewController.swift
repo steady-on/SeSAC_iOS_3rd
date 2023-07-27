f501684731c8035508344f2c60ef101c886be044
steady-on <roen.steady.on@gmail.com>
2023-07-27 23:30:57 +0900

    feat(Case3): tableView 관련 메서드
    - extension으로 UIViewController에 UITableViewDataSource를 채택
    - tableView를 그리기 위한 메서드 작성

--- Day009/PracticeTableView/PracticeTableView/Case3/Case3ViewController.swift
+++ Day009/PracticeTableView/PracticeTableView/Case3/Case3ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class Case3ViewController: UIViewController {
+    
+    @IBOutlet weak var todoTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     
     
+}
+
+extension UIViewController: UITableViewDataSource {
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        return TodoSectionType.allCases.count
+    }
+    
+    public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let sectionType = TodoSectionType(rawValue: section) else { return nil }
+        
+        return sectionType.title
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let sectionType = TodoSectionType(rawValue: section) else { return 0 }
+        
+        switch sectionType {
+        case .unDone:
+            return TodoManager.shared.unDoneTodo.count
+        case .done:
+            return TodoManager.shared.doneTodo.count
+        }
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let sectionType = TodoSectionType(rawValue: indexPath.section),
+              let unDoneTodoCell = tableView.dequeueReusableCell(withIdentifier: "unDoneTodoCell"),
+              let doneTodoCell = tableView.dequeueReusableCell(withIdentifier: "doneTodoCell") else { return UITableViewCell() }
+              
+        switch sectionType {
+        case .unDone:
+            return unDoneTodoCell
+        case .done:
+            return doneTodoCell
+        }
+    }
 }
