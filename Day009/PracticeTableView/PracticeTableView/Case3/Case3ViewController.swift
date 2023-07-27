904e83cd141ecb98bae986db23bc1c8a2a604784
steady-on <roen.steady.on@gmail.com>
2023-07-27 17:55:01 +0900

    add(Case3): initial files

---
+++ Day009/PracticeTableView/PracticeTableView/Case3/Case3ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Case3ViewController.swift
+//  PracticeTableView
+//
+//  Created by Roen White on 2023/07/27.
+//
+
+import UIKit
+
+class Case3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
