8bae93436cde86053c4f37388904f9af49ba2c54
steady-on <roen.steady.on@gmail.com>
2023-07-28 11:34:04 +0900

    refactor(Case3): TodoManager
    - todosStore에 @TodoStorage 프로퍼티 래퍼를 적용
    - todosStore가 옵셔널 타입으로 변경에 대응한 메서드 수정

--- Day009/PracticeTableView/PracticeTableView/Case3/TodoManager.swift
+++ Day009/PracticeTableView/PracticeTableView/Case3/TodoManager.swift
@@ -10,51 +10,56 @@ import Foundation
 final class TodoManager {
     static var shared = TodoManager()
     
-    private var todosStore = [Todo]()
+    @TodoStorage(key: "todosStore") private var todosStore: [Todo]?
     
     var todoCount: Int {
-        TodoManager.shared.todosStore.count
+        guard let todosStore = TodoManager.shared.todosStore else { return 0 }
+        return todosStore.count
     }
     
     var doneTodo: [Todo] {
-        TodoManager.shared.todosStore.filter { $0.isDone == true }
+        guard let todosStore = TodoManager.shared.todosStore else { return [Todo]() }
+        return todosStore.filter { $0.isDone == true }
     }
     
     var unDoneTodo: [Todo]  {
-        TodoManager.shared.todosStore.filter { $0.isDone == false }
+        guard let todosStore = TodoManager.shared.todosStore else { return [Todo]() }
+        return todosStore.filter { $0.isDone == false }
     }
     
     private init() {}
     
     static func add(_ todo: String) {
+        guard let todosStore = TodoManager.shared.todosStore else { return }
         let newTodo = Todo(todo: todo)
-        shared.todosStore.insert(newTodo, at: 0)
+
+        shared.todosStore?.insert(newTodo, at: 0)
     }
     
     static func delete(_ todoId: UUID) {
-        guard let selectedTodo = shared.todosStore.firstIndex(where: { $0.id == todoId }) else {
+        guard let selectedTodo = shared.todosStore?.firstIndex(where: { $0.id == todoId }) else {
             print("잘못된 요청입니다.")
             return
         }
         
-        shared.todosStore.remove(at: selectedTodo)
+        shared.todosStore?.remove(at: selectedTodo)
     }
     
     static func toggleIsDone(_ todoId: UUID) {
-        guard let selectedTodoIndex = shared.todosStore.firstIndex(where: { $0.id == todoId }) else {
+        guard let selectedTodoIndex = shared.todosStore?.firstIndex(where: { $0.id == todoId }) else {
             print("잘못된 요청입니다.")
             return
         }
         
-        shared.todosStore[selectedTodoIndex].isDone.toggle()
+        shared.todosStore?[selectedTodoIndex].isDone.toggle()
     }
     
     static func modify(_ todoId: UUID, string: String) {
-        guard let selectedTodoIndex = shared.todosStore.firstIndex(where: { $0.id == todoId }) else {
+        guard let selectedTodoIndex = shared.todosStore?.firstIndex(where: { $0.id == todoId }) else {
             print("잘못된 요청입니다.")
             return
         }
         
-        shared.todosStore[selectedTodoIndex].todo = string
+        shared.todosStore?[selectedTodoIndex].todo = string
     }
 }
