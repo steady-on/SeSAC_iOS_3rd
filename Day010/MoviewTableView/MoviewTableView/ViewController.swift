8a3099d306127807033c0460603b35441f27555c
steady-on <roen.steady.on@gmail.com>
2023-07-29 16:04:04 +0900

    design: searchTextField

--- Day010/MoviewTableView/MoviewTableView/ViewController.swift
+++ Day010/MoviewTableView/MoviewTableView/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
 }
 
+extension ViewController {
+    func designSearchTextField() {
+        searchTextField.font = .preferredFont(forTextStyle: .body)
+        searchTextField.textAlignment = .left
+        searchTextField.placeholder = "영화 제목을 입력해주세요."
+        searchTextField.borderStyle = .none
+        searchTextField.clearButtonMode = .always
+        searchTextField.autocapitalizationType = .none
+        searchTextField.autocorrectionType = .no
+        searchTextField.returnKeyType = .go
+    }
+}
