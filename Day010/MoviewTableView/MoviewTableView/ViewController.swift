eb1b8891572836aab2209bd5725c7065ab9a4680
steady-on <roen.steady.on@gmail.com>
2023-07-29 17:24:03 +0900

    fix: cell Identifier

--- Day010/MoviewTableView/MoviewTableView/ViewController.swift
+++ Day010/MoviewTableView/MoviewTableView/ViewController.swift
@@ -58,7 +58,7 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell") as? MovieTableViewCell else { return UITableViewCell() }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "movieInfo") as? MovieTableViewCell else { return UITableViewCell() }
         
         return cell
     }
