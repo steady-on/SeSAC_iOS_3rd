717d7ba53d069c4dadd6e8a19465d27672e9af1d
steady-on <roen.steady.on@gmail.com>
2023-07-29 15:34:42 +0900

    init: add new Project

---
+++ Day010/MoviewTableView/MoviewTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviewTableView
+//
+//  Created by Roen White on 2023/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
