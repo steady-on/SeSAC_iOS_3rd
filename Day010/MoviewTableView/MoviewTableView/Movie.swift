a714fb4e37dd6c4298e15c1408eb3740fa90640b
steady-on <roen.steady.on@gmail.com>
2023-07-29 16:55:01 +0900

    add: Data model
    - 영화의 데이터를 담을 Movie 구조체 작성
    - 영화들의 데이터를 모은 MovieStore 구조체 작성

---
+++ Day010/MoviewTableView/MoviewTableView/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  MoviewTableView
+//
+//  Created by Roen White on 2023/07/29.
+//
+
+import Foundation
+
+struct Movie {
+    let title: String
+    let releaseDate: String
+    let runtime: Int
+    let overview: String
+    let rate: Double
+}
