ae3feb2c245c949f7b66c6fe63e582e41227643c
steady-on <roen.steady.on@gmail.com>
2023-07-31 21:10:58 +0900

    chore: Book #1
    - Book의 데이터 속성을 추가함에 따라 Book의 프로퍼티 추가

--- Day011/Bookworm/Bookworm/Book.swift
+++ Day011/Bookworm/Bookworm/Book.swift
@@ -10,4 +10,20 @@ import Foundation
 struct Book: Codable {
     var title: String
     var author: String
+    var stateOfReading: StateOfReading.RawValue
+    var isBookmark: Bool
+}
+
+enum StateOfReading: Int {
+    case notYet
+    case reading
+    case finished
+    
+    var expression: String {
+        switch self {
+        case .notYet: return "아직 안 읽음"
+        case .reading: return "읽는 중"
+        case .finished: return "다 읽음"
+        }
+    }
 }
