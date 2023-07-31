cf91a4fc8a487dd635f53f84a3456afd353bfa31
steady-on <roen.steady.on@gmail.com>
2023-07-31 19:37:06 +0900

    design: set up view layout from code #1

--- Day011/Bookworm/Bookworm/ViewController.swift
+++ Day011/Bookworm/Bookworm/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate {
 
+    @IBOutlet weak var bookCollectionView: UICollectionView!
+    
+    let temp = ["Booktitle", "Author", "안 읽음"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        bookCollectionView.delegate = self
+        bookCollectionView.dataSource = self
+        
+        let nib = UINib(nibName: "BookCollectionViewCell", bundle: nil)
+        bookCollectionView.register(nib, forCellWithReuseIdentifier: "BookCollectionViewCell")
+        
+        setCollectionViewLayout()
     }
-
-
+    
+    func setCollectionViewLayout() {
+        let layout = UICollectionViewFlowLayout()
+        
+        let spacing: CGFloat = 10
+        let width = UIScreen.main.bounds.width - (spacing * 4)
+        
+        layout.itemSize = CGSize(width: width/3, height: width/3 * 1.5)
+        
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        bookCollectionView.collectionViewLayout = layout
+    }
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = bookCollectionView.dequeueReusableCell(withReuseIdentifier: "BookCollectionViewCell", for: indexPath) as? BookCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.configureBookCell()
+        cell.titleLabel.text = temp[0]
+        cell.authorLabel.text = temp[1]
+        cell.stateOfReading.text = temp[2]
+        
+        return cell
+    }
+    
+    
+}
