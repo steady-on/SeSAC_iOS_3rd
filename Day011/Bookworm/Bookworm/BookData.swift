9fff83ce75bd4fc3a364d652d8e15b62788859cb
steady-on <roen.steady.on@gmail.com>
2023-07-31 20:09:33 +0900

    feat: JSON 파일을 Book으로 변환

---
+++ Day011/Bookworm/Bookworm/BookData.swift
@@ -0,0 +1,30 @@
+//
+//  BookData.swift
+//  Bookworm
+//
+//  Created by Roen White on 2023/07/31.
+//
+
+import Foundation
+
+var bookData: [Book] = loadJson("BookData.json")
+
+func loadJson<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        fatalError("\(filename) not found")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Could not load \(filename): \(error)")
+    }
+    
+    do {
+        return try JSONDecoder().decode(T.self, from: data)
+    } catch {
+        fatalError("Unable to parse \(filename): \(error)")
+    }
+}
