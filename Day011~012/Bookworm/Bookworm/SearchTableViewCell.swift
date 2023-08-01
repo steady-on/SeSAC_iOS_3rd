2073812f3efbe4c766f9f95057543152b7237505
steady-on <roen.steady.on@gmail.com>
2023-08-02 02:57:03 +0900

    design: SearchTableViewCell #2
    - components들의 코드 연결 및 데이터 전달

--- Day011~012/Bookworm/Bookworm/SearchTableViewCell.swift
+++ Day011~012/Bookworm/Bookworm/SearchTableViewCell.swift
@@ -8,6 +8,28 @@
 import UIKit
 
 class SearchTableViewCell: UITableViewCell {
-
     
+    var book: Book?
+
+    @IBOutlet weak var backgroundUIView: UIView!
+    @IBOutlet weak var bookTitleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var introduceTextView: UITextView!
+    
+    func configureCell() {
+        guard let book else { return }
+        
+        self.backgroundColor = .systemGray6
+        
+        backgroundUIView.backgroundColor = .systemBackground
+        backgroundUIView.layer.cornerRadius = 15
+        backgroundUIView.clipsToBounds = true
+        
+        bookTitleLabel.text = book.title
+        authorLabel.text = book.author
+        introduceTextView.text = book.introduce
+        
+        introduceTextView.textContainer.maximumNumberOfLines = 3
+        introduceTextView.textContainer.lineBreakMode = .byTruncatingTail
+    }
 }
