c6202e8e626f5b7aa4a2bcdca1f061f551b66319
steady-on <roen.steady.on@gmail.com>
2023-08-02 03:11:13 +0900

    design(SearchTableViewCell): connect to SearchView #2

--- Day011~012/Bookworm/Bookworm/SearchViewController.swift
+++ Day011~012/Bookworm/Bookworm/SearchViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDelegate {
+    
+    private var searchKeyword: String?
+    private var filteredData: [Book] {
+        guard let searchKeyword else { return bookData }
+        return bookData.filter { $0.title.contains(searchKeyword) }
+    }
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var searchTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
+        
+        let nib = UINib(nibName: "SearchTableViewCell", bundle: nil)
+        searchTableView.register(nib, forCellReuseIdentifier: "SearchTableViewCell")
         
         setNavigationThing()
     }
@@ -29,3 +41,17 @@ class SearchViewController: UIViewController {
     }
 }
 
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = searchTableView.dequeueReusableCell(withIdentifier: "SearchTableViewCell") as? SearchTableViewCell else { return UITableViewCell() }
+        
+        cell.book = filteredData[indexPath.row]
+        cell.configureCell()
+        
+        return cell
+    }
+}
