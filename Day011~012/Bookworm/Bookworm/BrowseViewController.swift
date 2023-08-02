79451d8668e7892ae03653aac08de7f8222a352b
steady-on <roen.steady.on@gmail.com>
2023-08-02 18:36:45 +0900

    feat(BrowseViewController): set recentCollectionView #3
    - collectionView setting에 필요한 UICollectionViewDelegate, UICollectionViewDataSource 프로토콜 채택
    - UICollectionViewDataSource 프로토콜 관련 기본 메서드 작성
    - recentCollectionView의 delegate와 dataSource를 연결하고 사용할 cell을 등록하는 명령어를 모아 메서드로 작성

--- Day011~012/Bookworm/Bookworm/BrowseViewController.swift
+++ Day011~012/Bookworm/Bookworm/BrowseViewController.swift
@@ -8,22 +8,40 @@
 import UIKit
 
 class BrowseViewController: UIViewController {
+    
+    let data = bookData.shuffled()
 
+    @IBOutlet weak var recentCollectionView: UICollectionView!
+    @IBOutlet weak var bestTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureCollectionView()
+    }
+
+}
+
+extension BrowseViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func configureCollectionView() {
+        recentCollectionView.delegate = self
+        recentCollectionView.dataSource = self
+        
+        let nib = UINib(nibName: "BrowseCollectionViewCell", bundle: nil)
+        recentCollectionView.register(nib, forCellWithReuseIdentifier: "BrowseCollectionViewCell")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = recentCollectionView.dequeueReusableCell(withReuseIdentifier: "BrowseCollectionViewCell", for: indexPath) as? BrowseCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.book = data[indexPath.row]
+        cell.configureCell()
+        
+        return cell
+    }
 }
