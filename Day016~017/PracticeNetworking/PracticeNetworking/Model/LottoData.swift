6a180f7d6eea58e5043e8a113392ddc2635f979a
steady-on <roen.steady.on@gmail.com>
2023-08-09 01:38:14 +0900

    add(LottoData): json을 decoding하기위한 struct #5

---
+++ Day016~017/PracticeNetworking/PracticeNetworking/Model/LottoData.swift
@@ -0,0 +1,27 @@
+//
+//  LottoData.swift
+//  PracticeNetworking
+//
+//  Created by Roen White on 2023/08/09.
+//
+
+import Foundation
+
+struct LottoData: Codable {
+    var totSellamnt    : Int
+    var returnValue    : String
+    var drwNoDate      : String
+    var firstWinamnt   : Int
+    var drwtNo1        : Int
+    var drwtNo2        : Int
+    var drwtNo3        : Int
+    var drwtNo4        : Int
+    var drwtNo5        : Int
+    var drwtNo6        : Int
+    var bnusNo         : Int
+    var firstPrzwnerCo : Int
+    var firstAccumamnt : Int
+    var drwNo          : Int
+}
+
+
