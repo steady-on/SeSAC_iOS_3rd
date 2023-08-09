fcac8f6bcb4b68826a2842689d00b1194114673d
steady-on <roen.steady.on@gmail.com>
2023-08-09 16:03:16 +0900

    feat: TextField에 UIPicker 추가 #5
    - textField에 UIPicker를 추가하여 picker를 통해 회차를 선택하고, 바로 해당 회차의 데이터를 가져와 view에 보여지도록 함

--- Day016~017/PracticeNetworking/PracticeNetworking/LottoViewController.swift
+++ Day016~017/PracticeNetworking/PracticeNetworking/LottoViewController.swift
@@ -11,6 +11,9 @@ class LottoViewController: UIViewController {
     
     let lottoManager = LottoManager()
     
+    let drawingNumberPicker = UIPickerView()
+    let drawingNumbers: [Int] = Array(1...1079).reversed()
+    
     @IBOutlet var wrappingViews: [UIView]!
     
     @IBOutlet weak var drawingNumberTextField: UITextField!
@@ -24,7 +27,14 @@ class LottoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpDesignForUI()
+        
+        drawingNumberPicker.delegate = self
+        drawingNumberPicker.dataSource = self
+        
         drawingNumberTextField.text = "\(1079)"
+        drawingNumberTextField.inputView = drawingNumberPicker
+        drawingNumberTextField.tintColor = .clear
+        
         requestLotto()
     }
     
@@ -79,5 +89,24 @@ extension LottoViewController {
         label.shadowOffset = CGSize(width: 0.5, height: 0.5)
         label.layer.cornerRadius =  label.frame.width/2
         label.layer.masksToBounds = true
+    }
+}
+
+extension LottoViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return drawingNumbers.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(drawingNumbers[row])"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        drawingNumberTextField.text = "\(drawingNumbers[row])"
+        requestLotto()
     }
 }
