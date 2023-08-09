34f49303bf53709147f4b709a2cd62edcad790af
steady-on <roen.steady.on@gmail.com>
2023-08-09 09:21:00 +0900

    add: struct LottoDataManager #5
    - openAPI에 request를 보내고 받은 response를 LottoData → Lotto로 변환하는 구조체 작성

---
+++ Day016~017/PracticeNetworking/PracticeNetworking/Model/LottoDataManager.swift
@@ -0,0 +1,59 @@
+//
+//  LottoDataManager.swift
+//  PracticeNetworking
+//
+//  Created by Roen White on 2023/08/09.
+//
+
+import Foundation
+
+struct LottoDataManager {
+
+    private let lottoURL: String = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber"
+    
+    func fetchLotto(drawingNumber: Int, completion: @escaping (Lotto?) -> ()) {
+        let urlString = "\(lottoURL)&drwNo=\(drawingNumber)"
+        performRequest(with: urlString) { lotto in
+            completion(lotto)
+        }
+    }
+    
+    private func performRequest(with urlString: String, completion: @escaping (Lotto?) -> ()) {
+        
+        guard let url = URL(string: urlString) else { return }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { (data, _, error) in
+            if error != nil {
+                print(#function, "error :", error!)
+                completion(nil)
+                return
+            }
+            
+            guard let data, let lotto = self.parseJSON(data) else {
+                completion(nil)
+                return
+            }
+            
+            completion(lotto)
+        }
+        
+        task.resume()
+    }
+    
+    private func parseJSON(_ jsonData: Data) -> Lotto? {
+        let decoder = JSONDecoder()
+        
+        do {
+            let lottoData = try decoder.decode(LottoData.self, from: jsonData)
+            
+            let lotteryNumber = [lottoData.drwtNo1, lottoData.drwtNo2, lottoData.drwtNo3, lottoData.drwtNo4, lottoData.drwtNo5, lottoData.drwtNo6]
+            
+            let lotto = Lotto(drawingNumber: lottoData.drwNo, drawingDate: lottoData.drwNoDate, loteryNumber: lotteryNumber, bonusNumber: lottoData.bnusNo)
+            
+            return lotto
+        } catch {
+            print("Fail Parsing")
+            return nil
+        }
+    }
+}
