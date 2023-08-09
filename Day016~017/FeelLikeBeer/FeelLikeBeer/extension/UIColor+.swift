580e1ca496e77acfd3de7e90267f418290f827c1
steady-on <roen.steady.on@gmail.com>
2023-08-09 20:49:42 +0900

    add(color assets): UIColor에 static 변수로 추가 #5

---
+++ Day016~017/FeelLikeBeer/FeelLikeBeer/extension/UIColor+.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+.swift
+//  FeelLikeBeer
+//
+//  Created by Roen White on 2023/08/09.
+//
+
+import UIKit
+
+extension UIColor {
+    static var accentColor: UIColor { UIColor(named: "AccentColor")! }
+    static var backgroundColor: UIColor { UIColor(named: "BackgroundColor")! }
+    static var blockColor: UIColor { UIColor(named: "BlockColor")! }
+    static var fontColor: UIColor { UIColor(named: "FontColor")! }
+    static var pointColor: UIColor { UIColor(named: "PointColor")! }
+}
