9f160dd0d3d3dbb5e8fa00e51edc1539bbf5df44
steady-on <roen.steady.on@gmail.com>
2023-08-09 09:25:48 +0900

    design(LottoViewController): connect & design Component to viewController #5

--- Day016~017/PracticeNetworking/PracticeNetworking/LottoViewController.swift
+++ Day016~017/PracticeNetworking/PracticeNetworking/LottoViewController.swift
@@ -8,14 +8,47 @@
 import UIKit
 
 class LottoViewController: UIViewController {
+    
+    @IBOutlet var wrappingViews: [UIView]!
+    
+    @IBOutlet weak var drawingNumberTextField: UITextField!
+    @IBOutlet weak var drawingNumberLabel: UILabel!
+    @IBOutlet weak var drawingDate: UILabel!
+    
+    @IBOutlet var lotteryNumberButtons: [UIButton]!
+    @IBOutlet weak var bonusNumberButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        setUpDesignForUI()
         
     }
-
     
+    func setUpDesignForUI() {
+        designWrappingViews()
+        lotteryNumberButtons.forEach { designNumberButton($0) }
+        designNumberButton(bonusNumberButton)
+    }
+    
+    func configureButton(_ button: UIButton, for number: Int) {
+        let imageName = ""
+        
+        button.setTitle("\(number)", for: .disabled)
+        button.setBackgroundImage(UIImage(named: imageName), for: .disabled)
+    }
 }
 
+extension LottoViewController {
+    func designWrappingViews() {
+        wrappingViews.forEach {
+            $0.setBackgroundColor()
+            $0.setCornerRadius()
+        }
+    }
+    
+    func designNumberButton(_ button: UIButton) {
+        button.isEnabled = true
+        button.setTitleColor(.white, for: .disabled)
+    }
+}
