503244cfb0f7481f8095ef76227c7a00d0ef8473
steady-on <roen.steady.on@gmail.com>
2023-08-10 21:50:50 +0900

    feat: 받아온 데이터를 collectionView에 표현 #5

--- Day016~017/FeelLikeBeer/FeelLikeBeer/BeerCollectionViewController.swift
+++ Day016~017/FeelLikeBeer/FeelLikeBeer/BeerCollectionViewController.swift
@@ -9,22 +9,54 @@ import UIKit
 
 class BeerCollectionViewController: UICollectionViewController {
 
+    let beerManager = BeerManager()
+    var beers = [Beer]()
+    
+    @IBOutlet var beerCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureCollectionViewFlowLayout()
+        requestCall()
         
+    }
+    
+    func requestCall() {
+        beerManager.fetchPagingBeerData { beers in
+            self.beers.append(contentsOf: beers)
+            self.beerCollectionView.reloadData()
+        }
+    }
+    
+    func configureCollectionViewFlowLayout() {
+        let layout = UICollectionViewFlowLayout()
+        
+        let spacing: CGFloat = 20
+        let width = UIScreen.main.bounds.width - (spacing * 3)
+        
+        layout.itemSize = CGSize(width: width/2, height: width)
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        beerCollectionView.collectionViewLayout = layout
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        return 0
+        return beers.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BeerCollectionViewCell", for: indexPath) as? BeerCollectionViewCell else { return UICollectionViewCell() }
     
-        // Configure the cell
+        let beer = beers[indexPath.row]
+        
+        cell.nameLabel.text = beer.name
+        cell.descriptionTextView.text = beer.description
+        beer.getBeerImage { image in
+            DispatchQueue.main.async {
+                cell.imageView.image = image
+            }
+        }
     
         return cell
     }
