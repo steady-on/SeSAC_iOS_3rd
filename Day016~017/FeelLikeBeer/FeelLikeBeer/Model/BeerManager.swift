6861219ddd6fa5654f2832e75d076272fddddbce
steady-on <roen.steady.on@gmail.com>
2023-08-10 20:55:36 +0900

    feat(BeerManager): fetchPagingBeerData 메서드 추가 #5
    - paging된 데이터를 가져올 수 있는 메서드
    - static 프로퍼티로 page를 추가하고, page가 13을 넘지 않는 한 1씩 더해주는 메서드도 추가

--- Day016~017/FeelLikeBeer/FeelLikeBeer/Model/BeerManager.swift
+++ Day016~017/FeelLikeBeer/FeelLikeBeer/Model/BeerManager.swift
@@ -10,10 +10,16 @@ import Alamofire
 import SwiftyJSON
 
 struct BeerManager {
-    private let url = "https://api.punkapi.com/v2/beers/"
+    private let url = "https://api.punkapi.com/v2/beers"
+    static private var page = 1
+    
+    mutating func goToNextPage() {
+        guard BeerManager.page < 13 else { return }
+        BeerManager.page += 1
+    }
     
     func fetchRandomBeer(completion: @escaping (Beer) -> ()) {
-        let url = url + "random"
+        let url = url + "/random"
         AF.request(url, method: .get).validate().responseJSON { response in
             switch response.result {
             case .success(let value):
@@ -28,6 +34,35 @@ struct BeerManager {
                 let beer = Beer(name: name, description: description, pairingFoods: pairingFoods, tip: tip, imageUrl: imageUrl)
                 
                 completion(beer)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func fetchPagingBeerData(completion: @escaping ([Beer]) -> ()) {
+        let parameters: Parameters = ["page": "\(BeerManager.page)"]
+        
+        AF.request(url, method: .get, parameters: parameters).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value).arrayValue
+                var beers = [Beer]()
+                
+                for item in json {
+                    let name = item["name"].stringValue
+                    let description = item["description"].stringValue
+                    let pairingFoods = item["food_pairing"].arrayValue.map { $0.stringValue }
+                    let tip = item["brewers_tips"].stringValue
+                    let imageUrl = item["image_url"].stringValue
+                    
+                    let beer = Beer(name: name, description: description, pairingFoods: pairingFoods, tip: tip, imageUrl: imageUrl)
+                    
+                    beers.append(beer)
+                }
+                
+                completion(beers)
                 
             case .failure(let error):
                 print(error)
