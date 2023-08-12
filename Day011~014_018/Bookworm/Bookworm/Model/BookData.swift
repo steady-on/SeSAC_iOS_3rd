ecaa1b3937d8d033fc5a045582e5d5e2200b74ed
steady-on <roen.steady.on@gmail.com>
2023-08-12 17:10:57 +0900

    add: kakaoAPI data를 담을 BookData 구조체 생성 #6

---
+++ Day011~014_018/Bookworm/Bookworm/Model/BookData.swift
@@ -0,0 +1,43 @@
+//
+//  BookData.swift
+//  Bookworm
+//
+//  Created by Roen White on 2023/08/11.
+//
+
+import Foundation
+
+struct BookData: Codable {
+    let documents: [Document]
+    let meta: Meta
+}
+
+struct Document: Codable {
+    let authors: [String]
+    let contents, datetime, isbn: String
+    let price: Int
+    let publisher: String
+    let salePrice: Int
+    let status: String
+    let thumbnail: String
+    let title: String
+    let translators: [String]
+    let url: String
+
+    enum CodingKeys: String, CodingKey {
+        case authors, contents, datetime, isbn, price, publisher
+        case salePrice = "sale_price"
+        case status, thumbnail, title, translators, url
+    }
+}
+
+struct Meta: Codable {
+    let isEnd: Bool
+    let pageableCount, totalCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case isEnd = "is_end"
+        case pageableCount = "pageable_count"
+        case totalCount = "total_count"
+    }
+}
