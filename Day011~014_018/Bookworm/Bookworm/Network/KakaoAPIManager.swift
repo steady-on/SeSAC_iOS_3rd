80158ccbd322ff4301089f22b09086d0ff8104b8
steady-on <roen.steady.on@gmail.com>
2023-08-12 17:11:28 +0900

    add: kakaoAPI와 통신할 KakaoAPIManager 구조체 생성 #6

---
+++ Day011~014_018/Bookworm/Bookworm/Network/KakaoAPIManager.swift
@@ -0,0 +1,68 @@
+//
+//  KakaoAPIManager.swift
+//  Bookworm
+//
+//  Created by Roen White on 2023/08/11.
+//
+
+import Foundation
+import UIKit
+
+struct KakaoAPIManager {
+    private let urlString = "https://dapi.kakao.com/v3/search/book"
+    
+    func searchBook(query: String, completion: @escaping ([Book]?) -> Void) {
+        guard var urlComponents = URLComponents(string: urlString) else { return }
+        let paramaters = URLQueryItem(name: "query", value: query)
+        urlComponents.queryItems = [paramaters]
+        
+        performRequest(with: urlComponents) { books in completion(books) }
+    }
+    
+    private func performRequest(with urlComponents: URLComponents, completion: @escaping ([Book]?) -> Void) {
+        guard let url = urlComponents.url else { return }
+
+        let session = URLSession(configuration: .default)
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("KakaoAK \(APIKey.kakaoAPIKey)", forHTTPHeaderField: "Authorization")
+        
+        let task = session.dataTask(with: request) { (data, _, error) in
+            guard error == nil else {
+                print(error!)
+                completion(nil)
+                return
+            }
+            
+            guard let safeData = data, let books = self.parseJSON(safeData) else {
+                completion(nil)
+                return
+            }
+            
+            completion(books)
+        }
+    }
+    
+    private func parseJSON(_ jsonData: Data) -> [Book]? {
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(BookData.self, from: jsonData)
+            let bookDatas = decodedData.documents
+            
+            let books = bookDatas.map { data in
+                Book(title: data.title, author: data.authors.joined(), introduce: data.contents, thumbnail: data.thumbnail)
+            }
+            
+            return books
+            
+        } catch {
+            print("Parsing Failed")
+            return nil
+        }
+    }
+}
+
+
+
+
