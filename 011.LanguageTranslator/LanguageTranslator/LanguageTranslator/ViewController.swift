c2cf8192c6f361125d9df3a56427f61561c3224f
steady-on <roen.steady.on@gmail.com>
2023-08-13 16:41:47 +0900

    feat: UITextField에 custom 적용 #7

--- 011.LanguageTranslator/LanguageTranslator/LanguageTranslator/ViewController.swift
+++ 011.LanguageTranslator/LanguageTranslator/LanguageTranslator/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+fileprivate enum WhichLang {
+    case source
+    case target
+}
 
+class ViewController: UIViewController {
+    
+    let sourceLanguagePicker = UIPickerView()
+    let targetLanguagePicker = UIPickerView()
+    
+    let sourceLanguage = SourceLanguageType.allCases
+    let targetLanguage = TargetLanguageType.allCases
+
+    @IBOutlet weak var sourceLanguagePickTextField: UITextField!
+    @IBOutlet weak var targetLanguagePickTextField: UITextField!
+    @IBOutlet weak var swapButton: UIButton!
+    
+    @IBOutlet weak var sourceTextView: UITextView!
+    @IBOutlet weak var targetTextView: UITextView!
+    @IBOutlet weak var translateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureUI()
     }
 
-
+    private func configureUI() {
+        configureTextField(sourceLanguagePickTextField, which: .source)
+        configureTextField(targetLanguagePickTextField, which: .target)
+    }
 }
+
+extension ViewController: UITextFieldDelegate {
+    private func configureTextField(_ textField: UITextField, which: WhichLang) {
+        textField.delegate = self
+        
+        switch which {
+        case .source:
+            textField.inputView = sourceLanguagePicker
+        case .target:
+            textField.inputView = targetLanguagePicker
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return false
+    }
+}
+
 
