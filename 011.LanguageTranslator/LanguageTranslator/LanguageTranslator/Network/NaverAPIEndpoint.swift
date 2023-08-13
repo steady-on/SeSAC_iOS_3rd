70df112d2f28a4610fb4c4dca99ddf8c366f271b
steady-on <roen.steady.on@gmail.com>
2023-08-13 14:16:01 +0900

    add: NaverAPIEndpoint 열거형 #7
    - 번역과 언어감지 두가지를 골라 쓸 수 있도록 만듦

---
+++ 011.LanguageTranslator/LanguageTranslator/LanguageTranslator/Network/NaverAPIEndpoint.swift
@@ -0,0 +1,24 @@
+//
+//  NaverAPIEndpoint.swift
+//  LanguageTranslator
+//
+//  Created by Roen White on 2023/08/13.
+//
+
+import Foundation
+
+enum NaverAPIEndpoint {
+    case translate
+    case detectLangs
+    
+    private static let baseURL = "https://openapi.naver.com/v1/papago/"
+    
+    private var endPoint: String {
+        switch self {
+        case .translate: return "n2mt"
+        case .detectLangs: return "detectLangs"
+        }
+    }
+    
+    var requestURL: String { NaverAPIEndpoint.baseURL + self.endPoint }
+}
