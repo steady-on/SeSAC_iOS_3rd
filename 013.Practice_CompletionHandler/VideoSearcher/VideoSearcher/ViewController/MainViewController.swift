ea923756c03373ca6e1995b1fe1f32d3b87a719d
steady-on <roen.steady.on@gmail.com>
2023-08-17 22:06:11 +0900

    feat: 검색어를 입력하면 data를 fetch 하도록 함 #9

--- 013.Practice_CompletionHandler/VideoSearcher/VideoSearcher/ViewController/MainViewController.swift
+++ 013.Practice_CompletionHandler/VideoSearcher/VideoSearcher/ViewController/MainViewController.swift
@@ -9,13 +9,30 @@ import UIKit
 
 class MainViewController: UIViewController, ReuseIdentifying {
 
-    private var videos = [Document]()
+    private var videos = [Video]() {
+        didSet { videoTableView.reloadData() }
+    }
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var videoTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureVideoTableView()
     }
+}
 
-
+extension MainViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let query = searchBar.text else { return }
+        KakaoAPIManager.search(for: query) { videos in
+            DispatchQueue.main.async {
+                self.videos = videos
+            }
+        } errorHandler: { error in
+            print(error)
+        }
+    }
 }
 
