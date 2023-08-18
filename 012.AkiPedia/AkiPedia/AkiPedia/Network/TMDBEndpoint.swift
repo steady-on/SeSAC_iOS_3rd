79a0ecb1070d0945758e9088abf86c7a54d24d23
steady-on <roen.steady.on@gmail.com>
2023-08-18 22:05:50 +0900

    add: enum TMDBEndpoint #8

---
+++ 012.AkiPedia/AkiPedia/AkiPedia/Network/TMDBEndpoint.swift
@@ -0,0 +1,46 @@
+//
+//  TMDBEndpoint.swift
+//  AkiPedia
+//
+//  Created by Roen White on 2023/08/18.
+//
+
+import Foundation
+
+enum TMDBEndpoint {
+    
+    private static var baseURL = "https://api.themoviedb.org/3"
+    
+    case genre(mediaType: MediaType)
+    case trending(mediaType: MediaType?, timeWindow: TimeWindow = .day) // nil이면 all
+    case detail(mediaType: MediaType, id: Int)
+    
+    private var rawValue: String {
+        switch self {
+        case .genre: return "genre"
+        case .trending: return "trending"
+        case .detail: return "detail"
+        }
+    }
+    
+    var url: String {
+        
+        let addedEndpointURLString = Self.baseURL + "/\(self.rawValue)/"
+        
+        switch self {
+        case .genre(let mediaType):
+            return addedEndpointURLString + "\(mediaType.rawValue)"
+        case .trending(let mediaType, let timeWindow):
+            return addedEndpointURLString + "\(mediaType?.rawValue ?? "all")/\(timeWindow.rawValue)"
+        case .detail(let mediaType, let id):
+            return addedEndpointURLString + "\(mediaType.rawValue)/\(id)"
+        }
+    }
+    
+    enum TimeWindow: String {
+        case day
+        case week
+    }
+}
+
+
