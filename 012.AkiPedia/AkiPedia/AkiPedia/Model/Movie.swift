67a503696c5fbb02e19abb8d5b8a4ab6c4e27f6f
steady-on <roen.steady.on@gmail.com>
2023-08-19 23:23:50 +0900

    refactor: Movie와 TV 구조체가 TMDBData의 Result 구조체를 받는 생성자를 추가 #8

--- 012.AkiPedia/AkiPedia/AkiPedia/Model/Movie.swift
+++ 012.AkiPedia/AkiPedia/AkiPedia/Model/Movie.swift
@@ -9,8 +9,8 @@ import Foundation
 
 struct Movie: MediaProtocol {
     let id: Int
-    let title, originalTitle: String
-    let originalLanguage: String
+    let title: String
+    let originalTitle, originalLanguage: String
     let releaseDate: String
     let genreIDs: [Int]
     let backdropPath, posterPath: String
@@ -20,5 +20,26 @@ struct Movie: MediaProtocol {
     let voteAverage: Double
     let overview: String
     let video: Bool
+    
+    init(data: Result) {
+        guard let title = data.title, let originalTitle = data.originalTitle,
+        let releaseDate = data.releaseDate, let video = data.video
+        else { return }
+        
+        self.id = data.id
+        self.title = title
+        self.originalTitle = originalTitle
+        self.originalLanguage = data.originalLanguage
+        self.releaseDate = releaseDate
+        self.genreIDs = data.genreIDS
+        self.backdropPath = data.backdropPath
+        self.posterPath = data.posterPath
+        self.popularity = data.popularity
+        self.adult = data.adult
+        self.voteCount = data.voteCount
+        self.voteAverage = data.voteAverage
+        self.overview = data.overview
+        self.video = video
+    }
 }
 
--- 012.AkiPedia/AkiPedia/AkiPedia/Model/TV.swift
+++ 012.AkiPedia/AkiPedia/AkiPedia/Model/TV.swift
@@ -9,8 +9,8 @@ import Foundation
 
 struct TV: MediaProtocol {
     let id: Int
-    let name, originalName: String
-    let originalLanguage: String
+    let name: String
+    let originalName, originalLanguage: String
     let originCountry: [String]
     let firstAirDate: String
     let genreIDs: [Int]
@@ -19,5 +19,26 @@ struct TV: MediaProtocol {
     let adult: Bool
     let voteCount: Int
     let voteAverage: Double
-    let overview, mediaType: String
+    let overview: String
+    
+    init(data: Result) {
+        guard let name = data.name, let originalName = data.originalName,
+                let originCountry = data.originCountry, let firstAirDate = data.firstAirDate
+        else { return }
+        
+        self.id = data.id
+        self.name = name
+        self.originalName = originalName
+        self.originalLanguage = data.originalLanguage
+        self.originCountry = originCountry
+        self.firstAirDate = firstAirDate
+        self.genreIDs = data.genreIDS
+        self.backdropPath = data.backdropPath
+        self.posterPath = data.posterPath
+        self.popularity = data.popularity
+        self.adult = data.adult
+        self.voteCount = data.voteCount
+        self.voteAverage = data.voteAverage
+        self.overview = data.overview
+    }
 }
