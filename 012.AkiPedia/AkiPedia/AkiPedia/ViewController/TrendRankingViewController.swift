662cce32ac5897d26b876e4004860c9b8f6c84fb
steady-on <roen.steady.on@gmail.com>
2023-08-21 15:15:46 +0900

    init: add Project & set basic settings #10
    - 새 프로젝트 생성
    - 앱의 target 기기 및 버전 설정
    - Dark 모드로만 사용하도록 설정
    - APIKey 파일 추가
    - 그 외 file grouping

---
+++ 012.AkiPedia/AkiPedia/AkiPedia/ViewController/TrendRankingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AkiPedia
+//
+//  Created by Roen White on 2023/08/21.
+//
+
+import UIKit
+
+class TrendRankingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
