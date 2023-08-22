32028d5a0c23b9f7606231ccfd8ddd82e71174b6
steady-on <roen.steady.on@gmail.com>
2023-08-22 17:03:42 +0900

    design: MainView에 각 exampleview로 이동할 수 있는 버튼 생성 #11

--- 014.Practice_CodebaseAutolayout&UI/CodebaseAutolayout&UI/CodebaseAutolayout&UI/ViewController.swift
+++ 014.Practice_CodebaseAutolayout&UI/CodebaseAutolayout&UI/CodebaseAutolayout&UI/ViewController.swift
@@ -8,8 +8,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var firstExampleViewButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Example 01", for: .normal)
+        button.backgroundColor = .systemCyan
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    lazy var secondExampleViewButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Example 02", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    lazy var thirdExampleViewButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Example 03", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemIndigo
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        
+        let viewItems = [firstExampleViewButton, secondExampleViewButton, thirdExampleViewButton]
+        viewItems.forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        setUpConstraints()
+    }
+    
+    func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            firstExampleViewButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            firstExampleViewButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            firstExampleViewButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            firstExampleViewButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            firstExampleViewButton.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 1/3)
+        ])
+        
+        NSLayoutConstraint.activate([
+            secondExampleViewButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            secondExampleViewButton.topAnchor.constraint(equalTo: firstExampleViewButton.bottomAnchor),
+            secondExampleViewButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            secondExampleViewButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            secondExampleViewButton.heightAnchor.constraint(equalTo: firstExampleViewButton.heightAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            thirdExampleViewButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            thirdExampleViewButton.topAnchor.constraint(equalTo: secondExampleViewButton.bottomAnchor),
+            thirdExampleViewButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            thirdExampleViewButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            thirdExampleViewButton.heightAnchor.constraint(equalTo: firstExampleViewButton.heightAnchor)
+        ])
     }
 }
 
