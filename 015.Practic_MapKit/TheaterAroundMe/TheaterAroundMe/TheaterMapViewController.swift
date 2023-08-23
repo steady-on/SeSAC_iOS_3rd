fe7bf741f892ba5c15a1e5b5a9b62c616598c671
steady-on <roen.steady.on@gmail.com>
2023-08-23 22:14:43 +0900

    design: warningButton & View #12

--- 015.Practic_MapKit/TheaterAroundMe/TheaterAroundMe/TheaterMapViewController.swift
+++ 015.Practic_MapKit/TheaterAroundMe/TheaterAroundMe/TheaterMapViewController.swift
@@ -26,7 +26,7 @@ class TheaterMapViewController: UIViewController {
         config.cornerStyle = .medium
         
         button.configuration = config
-        
+    
         return button
     }()
     
@@ -42,6 +42,34 @@ class TheaterMapViewController: UIViewController {
         
         button.configuration = config
         
+        
+        return button
+    }()
+    
+    lazy var warningStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fillProportionally
+        
+        return stackView
+    }()
+    
+    lazy var warningButton: UIButton = {
+        let button = UIButton()
+        
+        var config = UIButton.Configuration.plain()
+        config.title = "위치서비스가 꺼져 있음"
+        config.image = UIImage(systemName: "chevron.right")
+        config.imagePlacement = .trailing
+        config.imagePadding = 4
+        config.buttonSize = .mini
+        config.baseForegroundColor = .label
+        button.configuration = config
+
+        button.setTitleColor(.systemBlue, for: .highlighted)
+        
         return button
     }()
     
@@ -50,24 +78,33 @@ class TheaterMapViewController: UIViewController {
         view.backgroundColor = .systemBackground
         
         locationManager.delegate = self
-        mapView.delegate = self
         
-        [mapView, filterButton, hereButton].forEach {
+        [mapView, warningStackView, filterButton, hereButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview($0)
         }
-        
         setUpConstraints()
     }
 
-    func setUpConstraints() {
+    private func setUpConstraints() {
         NSLayoutConstraint.activate([
             mapView.topAnchor.constraint(equalTo: view.topAnchor),
             mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            warningStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            warningStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            warningStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+            
+        warningStackView.addArrangedSubview(warningButton)
         
-            filterButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+        NSLayoutConstraint.activate([
+            warningButton.topAnchor.constraint(equalTo: warningStackView.topAnchor),
+            warningButton.centerXAnchor.constraint(equalTo: warningStackView.centerXAnchor),
+            
+            filterButton.topAnchor.constraint(equalTo: warningButton.bottomAnchor, constant: 16),
             filterButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
             filterButton.heightAnchor.constraint(equalTo: hereButton.widthAnchor, multiplier: 1),
             
@@ -79,9 +116,5 @@ class TheaterMapViewController: UIViewController {
 }
 
 extension TheaterMapViewController: CLLocationManagerDelegate {
-    
-}
-
-extension TheaterMapViewController: MKMapViewDelegate {
     
 }
