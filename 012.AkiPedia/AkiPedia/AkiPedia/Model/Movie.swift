165fbfc5207e91c2ea9102483682c3fae801d7aa
steady-on <roen.steady.on@gmail.com>
2023-08-28 20:28:52 +0900

    add: MediaProtocol을 따르는 구조체에 Result type을 매개변수로 받는 생성자 추가 #10

--- 012.AkiPedia/AkiPedia/AkiPedia/Model/Movie.swift
+++ 012.AkiPedia/AkiPedia/AkiPedia/Model/Movie.swift
@@ -27,4 +27,26 @@ struct Movie: MediaProtocol {
     let title, originalTitle: String
     let releaseDate: String
     let video: Bool
+    
+    init(data: Result) {
+        guard let genreDictionary = Movie.genreDictionary else { return }
+        let genres = data.genreIDS.map { genreDictionary[$0] ?? "" }
+        
+        self.id = data.id
+        self.genres = genres
+        self.overview = data.overview
+        self.originalLanguage = data.originalLanguage
+        self.backdropPath = data.backdropPath
+        self.posterPath = data.posterPath
+        self.popularity = data.popularity
+        self.voteAverage = data.voteAverage
+        self.voteCount = data.voteCount
+        self.adult = data.adult
+        
+        /// distinct property
+        self.title = data.title ?? "정보없음"
+        self.originalTitle = data.originalTitle ?? "정보없음"
+        self.releaseDate = data.releaseDate ?? "정보없음"
+        self.video = data.video ?? false
+    }
 }
--- 012.AkiPedia/AkiPedia/AkiPedia/Model/Tv.swift
+++ 012.AkiPedia/AkiPedia/AkiPedia/Model/Tv.swift
@@ -27,4 +27,26 @@ struct Tv: MediaProtocol {
     let name, originalName: String
     let firstAirDate: String
     let originCountry: [String]
+    
+    init(data: Result) {
+        guard let genreDictionary = Tv.genreDictionary else { return }
+        let genres = data.genreIDS.map { genreDictionary[$0] ?? "" }
+        
+        self.id = data.id
+        self.genres = genres
+        self.overview = data.overview
+        self.originalLanguage = data.originalLanguage
+        self.backdropPath = data.backdropPath
+        self.posterPath = data.posterPath
+        self.popularity = data.popularity
+        self.voteAverage = data.voteAverage
+        self.voteCount = data.voteCount
+        self.adult = data.adult
+        
+        /// distinct property
+        self.name = data.name ?? "정보없음"
+        self.originalName = data.originalName ?? "정보없음"
+        self.firstAirDate = data.firstAirDate ?? "정보없음"
+        self.originCountry = data.originCountry ?? ["정보없음"]
+    }
 }
