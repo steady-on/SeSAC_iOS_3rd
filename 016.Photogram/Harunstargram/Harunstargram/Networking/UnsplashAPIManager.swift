087fb06a0d41f36260ac3c6ced1a78cd64a5c70a
steady-on <roen.steady.on@gmail.com>
2023-09-01 19:31:53 +0900

    feat(UnsplashAPIManager):  request를 수행하는 메서드 작성 #14

--- 016.Photogram/Harunstargram/Harunstargram/Networking/UnsplashAPIManager.swift
+++ 016.Photogram/Harunstargram/Harunstargram/Networking/UnsplashAPIManager.swift
@@ -7,7 +7,41 @@
 
 import Foundation
 
-final class UnsplashAPIManager {
+struct UnsplashAPIManager {
+    private static let headers = [
+        "Accept-Version": "v1",
+        "Authorization": "Client-ID \(APIKey.unsplashAccessKey)"
+    ]
+    
+    private static func performRequest(for urlComponents: URLComponents, completionHandler: @escaping ([UnsplashPhoto]?) -> Void) {
+        guard let url = urlComponents.url else { return }
+        
+        let session = URLSession.shared
+        var request = URLRequest(url: url, timeoutInterval: 10)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                completionHandler(nil)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                completionHandler(nil)
+                return
+            }
+            
+            guard let data, let decodeData = parseJSON(data) else {
+                completionHandler(nil)
+                return
+            }
+            
+            completionHandler(decodeData)
+        }
+    }
+    
     private static func parseJSON(_ jsonData: Data) -> [UnsplashPhoto]? {
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
