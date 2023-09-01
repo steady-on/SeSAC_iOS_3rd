9bf80cabc9edfed6e608c9fe43c442fe7bed1d40
steady-on <roen.steady.on@gmail.com>
2023-09-01 19:08:51 +0900

    add: unsplash api 사용을 위해 APIKey 파일 추가 및 decoding에 사용할 구조체 추가 #14

---
+++ 016.Photogram/Harunstargram/Harunstargram/Networking/UnsplashData.swift
@@ -0,0 +1,18 @@
+//
+//  UnsplashData.swift
+//  Harunstargram
+//
+//  Created by Roen White on 2023/09/01.
+//
+
+import Foundation
+
+struct UnsplashPhoto: Codable {
+    let id: String
+    let urls: Link
+}
+
+struct Link: Codable {
+    let thumb: String
+    let full: String
+}
