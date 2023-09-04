06d47d04fc17cc6c0a7bdb8996deef3ffd87053b
steady-on <roen.steady.on@gmail.com>
2023-09-04 16:36:48 +0900

    refactor(DetailViewController): MyBook 타입도 받아서 정보를 보여줄 수 있도록 수정 #15
    - 약간의 View 수정

--- 009.Bookworm/Bookworm/Bookworm/ViewControllers/DetailViewController.swift
+++ 009.Bookworm/Bookworm/Bookworm/ViewControllers/DetailViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
-    var book: Book?
     let placeholder = "내용을 입력해주세요(책속의 한줄/감상평 등)"
 
     @IBOutlet weak var contentView: UIView!
@@ -20,29 +19,52 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var introduceTextView: UITextView!
     @IBOutlet weak var userMemoTextView: UITextView!
     
+    var myBook: MyBook?
+    
+    var book: Book?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setDetailView()
+        configureView()
         userMemoTextView.delegate = self
+    }
+    
+    func configureView() {
+        if let myBook {
+            title = myBook.title
+            
+            backgroundImageView.loadData(url: myBook.thumbnail)
+            backgroundImageView.contentMode = .scaleAspectFill
+            backgroundImageView.applyBlurEffect()
+            
+            coverImageView.loadData(url: myBook.thumbnail)
+            
+            titleLabel.text = myBook.title
+            authorLabel.text = myBook.author
+            
+            introduceTextView.text = myBook.introduce
+        }
+        
+        if let book {
+            title = book.title
+
+            backgroundImageView.image = UIImage(named: book.title)
+            backgroundImageView.contentMode = .bottom
+            backgroundImageView.applyBlurEffect()
+            
+            coverImageView.image = UIImage(named: book.title)
+
+            titleLabel.text = book.title
+            authorLabel.text = book.author
+            
+            introduceTextView.text = book.introduce
+        }
     }
     
     func setDetailView() {
         contentView.layer.cornerRadius = 15
-        guard let book else { return }
-        
-        title = book.title
-
-        backgroundImageView.image = UIImage(named: book.title)
-        backgroundImageView.contentMode = .bottom
-        backgroundImageView.applyBlurEffect()
-        
-        coverImageView.image = UIImage(named: book.title)
-
-        titleLabel.text = book.title
-        authorLabel.text = book.author
-        
-        introduceTextView.text = book.introduce
         
         userMemoTextView.layer.borderWidth = 0.8
         userMemoTextView.layer.borderColor = UIColor.systemGray4.cgColor
