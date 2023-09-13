c471dbc5218a7cc22896827cdc0434c6102f2ec9
steady-on <roen.steady.on@gmail.com>
2023-09-13 19:04:44 +0900

    refactor: textField와 회차정보를 나타내는 label에 대한 로직을 viewModel로 이동 #16

--- 010.Practice_Networking/LotteryChecker/LotteryChecker/LottoViewModel.swift
+++ 010.Practice_Networking/LotteryChecker/LotteryChecker/LottoViewModel.swift
@@ -9,14 +9,26 @@ import Foundation
 
 final class LottoViewModel {
     
-    let selectedDrawingNumber = Observable("1084")
     
+    private let drawingNumbers = Array(1...1084).reversed().map { String($0) }
+    var numberOfDrawingNumbers: Int { drawingNumbers.count }
     
-    let drawingNumbers: [Int] = Array(1...1084).reversed()
+    let selectedNumber: Observable<String?> = Observable("1084")
+    let seletedDrawingNumberLable = Observable("1084회차 당첨 번호")
     
     func titleForRow(_ row: Int) -> String {
-        return "\(drawingNumbers[row])"
+        return drawingNumbers[row]
     }
     
+    func setPickerValueToSelectedNumber(for row: Int) {
+        selectedNumber.value = drawingNumbers[row]
+    }
     
+    func textForDrawingNumberLabel() {
+        guard let selectedNumber = selectedNumber.value else {
+            seletedDrawingNumberLable.value = "회차 정보가 올바르지 않습니다."
+            return
+        }
+        seletedDrawingNumberLable.value = "\(selectedNumber)회 당첨 번호"
+    }
 }
