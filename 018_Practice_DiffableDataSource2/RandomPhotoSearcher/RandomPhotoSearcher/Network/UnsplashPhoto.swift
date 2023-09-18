cfb19bd659fc6f12be03b7f5edc6900607963131
steady-on <roen.steady.on@gmail.com>
2023-09-19 01:21:47 +0900

    refactor: 사진 정보가 될 프로퍼티 추가 #18

--- 018_Practice_DiffableDataSource2/RandomPhotoSearcher/RandomPhotoSearcher/Network/UnsplashPhoto.swift
+++ 018_Practice_DiffableDataSource2/RandomPhotoSearcher/RandomPhotoSearcher/Network/UnsplashPhoto.swift
@@ -7,12 +7,18 @@
 
 import Foundation
 
-struct UnsplashPhoto: Codable {
+struct UnsplashPhoto: Codable, Hashable {
     let id: String
+    let description: String?
+    let location: Location?
     let urls: Link
 }
 
-struct Link: Codable {
+struct Link: Codable, Hashable {
     let thumb: String
     let full: String
+}
+
+struct Location: Codable, Hashable {
+    let name: String?
 }
