4cbdd9ed9c638e511eb38d051afe577ffbcf5b65
steady-on <roen.steady.on@gmail.com>
2023-09-19 00:38:14 +0900

    init: add project & basic settings #18

---
+++ 018_Practice_DiffableDataSource2/RandomPhotoSearcher/RandomPhotoSearcher/PhotoCollectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhotoSearcher
+//
+//  Created by Roen White on 2023/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
