b20084f6bf19ca30ecbeeb3981ed498e6d7c7d9d
steady-on <roen.steady.on@gmail.com>
2023-09-19 19:25:24 +0900

    fix: navigationController 적용 #18

--- 018_Practice_DiffableDataSource2/RandomPhotoSearcher/RandomPhotoSearcher/SceneDelegate.swift
+++ 018_Practice_DiffableDataSource2/RandomPhotoSearcher/RandomPhotoSearcher/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         let vc = PhotoCollectionViewController()
-        window?.rootViewController = vc
+        window?.rootViewController = UINavigationController(rootViewController: vc)
         window?.makeKeyAndVisible()
     }
 
