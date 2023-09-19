ede6b64924a1b28fe095acbcd58c647bbd23c0b9
steady-on <roen.steady.on@gmail.com>
2023-09-19 19:47:46 +0900

    add(PhotoViewModel): unsplashPhotos 프로퍼티와 random image를 받아오는 메서드 추가 #18

--- 018_Practice_DiffableDataSource2/RandomPhotoSearcher/RandomPhotoSearcher/ViewModel/PhotoViewModel.swift
+++ 018_Practice_DiffableDataSource2/RandomPhotoSearcher/RandomPhotoSearcher/ViewModel/PhotoViewModel.swift
@@ -8,5 +8,16 @@
 import Foundation
 
 final class PhotoViewModel {
+    let unsplashPhotos = Observable(value: [UnsplashPhoto]())
     
+    func fetchRandomImage(for query: String?) {
+        UnsplashAPIManager.fetchRandomImage(for: query) { [weak self] result in
+            guard let result else {
+                self?.unsplashPhotos.value = []
+                return
+            }
+            
+            self?.unsplashPhotos.value = result
+        }
+    }
 }
