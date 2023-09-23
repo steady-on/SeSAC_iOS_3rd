353551f768483e8b8150761cf6a1c66eb2e1fae8
steady-on <roen.steady.on@gmail.com>
2023-09-23 21:05:03 +0900

    refactor: appied responseDecodable method and add a method for getting a single beer #19

--- 010_Practice_Networking/FeelLikeBeer/FeelLikeBeer/Model/BeerManager.swift
+++ 010_Practice_Networking/FeelLikeBeer/FeelLikeBeer/Model/BeerManager.swift
@@ -13,60 +13,55 @@ struct BeerManager {
     private let url = "https://api.punkapi.com/v2/beers"
     static private var page = 1
     
+    private let decoder = {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }()
+    
     mutating func goToNextPage() {
         guard BeerManager.page < 13 else { return }
         BeerManager.page += 1
     }
     
-    func fetchRandomBeer(completion: @escaping (Beer) -> ()) {
-        let url = url + "/random"
-        AF.request(url, method: .get).validate().responseJSON { response in
+    // Get a Single Beer
+    func requestSpecifiedBeer(for id: Int, completion: @escaping (Result<Beer, Error>) -> Void) {
+        let url = url + "\(id)"
+        
+        AF.request(url, method: .get).responseDecodable(of: Beer.self, decoder: decoder) { response in
             switch response.result {
-            case .success(let value):
-                guard let json = JSON(value).arrayValue.first else { return }
-                
-                let name = json["name"].stringValue
-                let description = json["description"].stringValue
-                let pairingFoods = json["food_pairing"].arrayValue.map { $0.stringValue }
-                let tip = json["brewers_tips"].stringValue
-                let imageUrl = json["image_url"].stringValue
-                
-                let beer = Beer(name: name, description: description, foodPairing: pairingFoods, tip: tip, imageUrl: imageUrl)
-                
-                completion(beer)
-                
-            case .failure(let error):
-                print(error)
+            case .success(let data):
+                completion(.success(data))
+            case .failure(let failure):
+                completion(.failure(failure))
             }
         }
     }
     
-    func fetchPagingBeerData(completion: @escaping ([Beer]) -> ()) {
+    // Get Random Beer
+    func fetchRandomBeer(completion: @escaping (Result<Beer, Error>) -> ()) {
+        let url = url + "/random"
+        AF.request(url, method: .get).responseDecodable(of: Beer.self, decoder: decoder) { response in
+            switch response.result {
+            case .success(let data):
+                completion(.success(data))
+            case .failure(let failure):
+                completion(.failure(failure))
+            }
+        }
+    }
+    
+    // Get Beers + pagenation
+    func fetchPagingBeerData(completion: @escaping (Result<[Beer], Error>) -> ()) {
         
         let parameters: Parameters = ["page": "\(BeerManager.page)"]
         
-        AF.request(url, method: .get, parameters: parameters).validate().responseJSON { response in
+        AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding(destination: .queryString)).responseDecodable(of: [Beer].self, decoder: decoder) { response in
             switch response.result {
-            case .success(let value):
-                let json = JSON(value).arrayValue
-                var beers = [Beer]()
-                
-                for item in json {
-                    let name = item["name"].stringValue
-                    let description = item["description"].stringValue
-                    let foodPairing = item["food_pairing"].arrayValue.map { $0.stringValue }
-                    let tip = item["brewers_tips"].stringValue
-                    let imageUrl = item["image_url"].stringValue
-                    
-                    let beer = Beer(name: name, description: description, foodPairing: foodPairing, tip: tip, imageUrl: imageUrl)
-                    
-                    beers.append(beer)
-                }
-                
-                completion(beers)
-                
-            case .failure(let error):
-                print(error)
+            case .success(let data):
+                completion(.success(data))
+            case .failure(let failure):
+                completion(.failure(failure))
             }
         }
     }
